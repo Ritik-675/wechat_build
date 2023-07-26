015d1d131d7f689ac1672d92dfb5d789256ab4de
ritik675 <ritik.yadav.ug20@nsut.ac.in>
2023-07-27 01:39:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      {/* {header} */}
+      <Header />
+      {/* {side bar} */}
+      {/* {React-Router -> Chat Screen} */}
+    </div>
+  );
+}
+
+export default App;
